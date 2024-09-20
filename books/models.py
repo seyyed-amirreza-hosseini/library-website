4ae78e20217f11d06c6f6c3096737eb53ce0144b
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-20 21:44:02 +0330

    Create book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
