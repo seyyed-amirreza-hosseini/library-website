d9c645d0967d98c412f56fc062e30d00e2153f66
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-20 22:11:00 +0330

    Write tests for books app

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Book
 
-# Create your tests here.
+
+class BookTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="ML",
+            subtitle="ML",
+            author="Amirreza Hosseini",
+            isbn="12345678901234", 
+        )
+
+    def test_book_content(self):
+        self.assertEqual(self.book.title, "ML")
+        self.assertEqual(self.book.subtitle, "ML")    
+        self.assertEqual(self.book.author, "Amirreza Hosseini")    
+        self.assertEqual(self.book.isbn, "12345678901234")        
+
+    def test_book_listview(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hosseini")
+        self.assertTemplateUsed(response, "books/book_list.html")
