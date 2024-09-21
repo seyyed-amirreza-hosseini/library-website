9c297c640e94b8bf05741fe44d6de15ecbed2421
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-09-21 17:08:17 +0330

    Create an api for books list

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from books.models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookAPIView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+
--- library/urls.py
+++ library/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include('apis.urls')),
     path("", include('books.urls')),
 ]
